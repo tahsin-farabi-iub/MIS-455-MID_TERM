a6d8d1b28193f0c9b86034c6f6b9ed24ff26675a
tahsin-farabi-iub <2320120@iub.edu.bd>
2026-07-04 09:06:27 -0700

    updated js script

---
+++ script.js
@@ -0,0 +1,6 @@
+function subscribeNewsletter() {
+  alert("Thanks for subscribing to Gift Nest updates!");
+}
+function clickIMG() {
+  alert ("You have recieved an exclusive offer of 10% discount on your first purchase!");
+}
